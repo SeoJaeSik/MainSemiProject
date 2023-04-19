f4a01d12cf9da916bdd0f9e0491e7b7a7484db82
user <user@LAPTOP-O3K1PEA2>
2023-04-19 18:31:23 +0900

    용수진 브렌치 테스트ing

--- MainSemiProject/src/main/java/sujin/test/Sujin_test.java
+++ MainSemiProject/src/main/java/sujin/test/Sujin_test.java
@@ -5,6 +5,7 @@ public class Sujin_test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("용수진 브렌치 테스트");
+		System.out.println("제발!!!");
 	}
 
 }
