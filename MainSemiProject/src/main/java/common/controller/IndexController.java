112b644e9a5e0722561e18bc8413c0b7f0462261
yunhwan <yunhwan@localhost>
2023-04-21 18:35:30 +0900

    프론트컨트롤러 수정 및 우리조 프로젝트 웹사이트 띄움.

--- MainSemiProject/src/main/java/common/controller/IndexController.java
+++ MainSemiProject/src/main/java/common/controller/IndexController.java
@@ -1,23 +1,15 @@
 package common.controller;
 
-import java.sql.SQLException;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.SendResult;
-
 
 public class IndexController extends AbstractController {
 
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+   @Override
+   public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-			super.setRedirect(false);
-			super.setViewPage(request.getContextPath()+"/WEB-INF/jaesik/index.jsp");
-		}
-		// 익셉션 처리를 해주지 않으면 http 500 과같은 에러페이지를 유저가 볼 수 있다.
-		// 이를 바탕으로 해킹시도가 있을 수 있기 때문에 반드시 익셉션처리를 해줘야 한다.
-	
+      super.setRedirect(false);
+      super.setViewPage("/WEB-INF/jaesik/index.jsp");
+   }
 
 }
