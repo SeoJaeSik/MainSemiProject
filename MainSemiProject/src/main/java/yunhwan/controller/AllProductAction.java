17c8ec75d13a31df4adebaae3da5092da41d1e54
yunhwan <yunhwan@localhost>
2023-04-23 19:26:40 +0900

    불필요 테스트 파일 삭제, 전체제품페이지 css 업데이트

---
+++ MainSemiProject/src/main/java/yunhwan/controller/AllProductAction.java
@@ -0,0 +1,16 @@
+package yunhwan.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class AllProductAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
