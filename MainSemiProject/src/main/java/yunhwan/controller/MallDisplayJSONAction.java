4cbdc518909f145fac147c75a1e80ec1b2f598b6
yunhwan <yunhwan@localhost>
2023-04-25 17:06:40 +0900

    아작스로 제품목록 나타내기 구현 중 중간 저장

---
+++ MainSemiProject/src/main/java/yunhwan/controller/MallDisplayJSONAction.java
@@ -0,0 +1,89 @@
+package yunhwan.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import common.controller.AbstractController;
+import yunhwan.model.InterProductDAO;
+import yunhwan.model.ProductDAO;
+import yunhwan.model.ProductVO;
+
+public class MallDisplayJSONAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String shoes_category_no = request.getParameter("shoes_category_no"); // ajax에서 지정한 HIT 가 넘어옴 NEW도 가능
+		String start = request.getParameter("start");
+		String len = request.getParameter("len"); // end 를 구하기 위해 가져온 것이다.
+		
+		InterProductDAO pdao = new ProductDAO();
+		
+		Map<String, String> paraMap = new HashMap<>();
+		paraMap.put("category", shoes_category_no);
+		paraMap.put("start", start);  
+		
+		String end = String.valueOf(Integer.parseInt(start) + Integer.parseInt(len) - 1);
+		paraMap.put("end", end);  
+		
+		List<ProductVO> prodList = pdao.selectProduct(paraMap);
+		
+		System.out.println(prodList);
+		
+		JSONArray jsonArr = new JSONArray(); // []
+		
+		if( prodList.size() > 0 ) {
+			// DB에서 조회해 온 결과물이 있을 경우
+			
+			for(ProductVO pvo : prodList) {
+				
+				JSONObject jsonObj = new JSONObject(); 						// {}
+				jsonObj.put("pnum", pvo.getPnum()); 						// {"pnum":36, }
+				jsonObj.put("pname", pvo.getPname());						// {"pname":36, "pname":"노트북30"}
+				jsonObj.put("code", pvo.getCategvo().getCode());			// {"pname":36, "pname":"노트북30", "code":"100000"}
+				jsonObj.put("pcompany", pvo.getPcompany());					// {"pname":36, "pname":"노트북30", "code":"100000", "pcompany":"삼성전자"}
+				jsonObj.put("pimage1", pvo.getPimage1());					// 생략
+	            jsonObj.put("pimage2", pvo.getPimage2());
+	            jsonObj.put("pqty", pvo.getPqty());
+	            jsonObj.put("price", pvo.getPrice());
+	            jsonObj.put("saleprice", pvo.getSaleprice());
+	            jsonObj.put("sname", pvo.getSpvo().getSname());
+	            jsonObj.put("pcontent", pvo.getPcontent());
+	            jsonObj.put("point", pvo.getPoint());
+	            jsonObj.put("pinputdate", pvo.getPinputdate());
+	            jsonObj.put("discoutPercent", pvo.getDiscountPercent());	// {"pname":36, "pname":"노트북30", "code":"100000", "pcompany":"삼성전자", ......, "pinputdate":"2023-04-19", "discoutPercent":17}
+				
+	            jsonArr.put(jsonObj); 	// [{}]
+	            						// [{},{}]
+	            						// [{},{},{}]
+			}// end of for--------------------
+		
+		}// end of if-------------------------
+
+		
+		String json = jsonArr.toString();			//  [ {},{},{}, ...... {} ]을 문자열로 변환하기
+		// "[ {},{},{}, ...... {} ]"
+
+//		System.out.println("~~~ 확인용 json => " + json);			//  [ {},{},{}, ...... {} ]을 문자열로 변환하기
+																// "[ {},{},{}, ...... {} ]"
+		// DB에서 조회해온 결과가 있을경우
+		// ~~~ 확인용 json => [{"pnum":36,"code":"100000","pname":"노트북30","pcompany":"삼성전자","saleprice":1000000,"discoutPercent":17,"point":60,"pinputdate":"2023-04-19 11:12:06","pimage1":"59.jpg","pqty":100,"pimage2":"60.jpg","pcontent":"30번 노트북","price":1200000,"sname":"HIT"},{"pnum":35,"code":"100000","pname":"노트북29","pcompany":"레노버","saleprice":1000000,"discoutPercent":17,"point":60,"pinputdate":"2023-04-19 11:12:06","pimage1":"57.jpg","pqty":100,"pimage2":"58.jpg","pcontent":"29번 노트북","price":1200000,"sname":"HIT"},{"pnum":34,"code":"100000","pname":"노트북28","pcompany":"아수스","saleprice":1000000,"discoutPercent":17,"point":60,"pinputdate":"2023-04-19 11:12:06","pimage1":"55.jpg","pqty":100,"pimage2":"56.jpg","pcontent":"28번 노트북","price":1200000,"sname":"HIT"},{"pnum":33,"code":"100000","pname":"노트북27","pcompany":"애플","saleprice":1000000,"discoutPercent":17,"point":60,"pinputdate":"2023-04-19 11:12:06","pimage1":"53.jpg","pqty":100,"pimage2":"54.jpg","pcontent":"27번 노트북","price":1200000,"sname":"HIT"},{"pnum":32,"code":"100000","pname":"노트북26","pcompany":"MSI","saleprice":1000000,"discoutPercent":17,"point":60,"pinputdate":"2023-04-19 11:12:06","pimage1":"51.jpg","pqty":100,"pimage2":"52.jpg","pcontent":"26번 노트북","price":1200000,"sname":"HIT"},{"pnum":31,"code":"100000","pname":"노트북25","pcompany":"삼성전자","saleprice":1000000,"discoutPercent":17,"point":60,"pinputdate":"2023-04-19 11:12:06","pimage1":"49.jpg","pqty":100,"pimage2":"50.jpg","pcontent":"25번 노트북","price":1200000,"sname":"HIT"},{"pnum":30,"code":"100000","pname":"노트북24","pcompany":"한성컴퓨터","saleprice":1000000,"discoutPercent":17,"point":60,"pinputdate":"2023-04-19 11:12:06","pimage1":"47.jpg","pqty":100,"pimage2":"48.jpg","pcontent":"24번 노트북","price":1200000,"sname":"HIT"},{"pnum":29,"code":"100000","pname":"노트북23","pcompany":"DELL","saleprice":1000000,"discoutPercent":17,"point":60,"pinputdate":"2023-04-19 11:12:06","pimage1":"45.jpg","pqty":100,"pimage2":"46.jpg","pcontent":"23번 노트북","price":1200000,"sname":"HIT"}]
+		
+		// ~~~ 확인용 json => []
+		// 만약에 select된 정보가 없다면 [] 로 나오므로 null이 아닌 요소가 없는 빈 배열이다.
+		
+		request.setAttribute("json", json);
+		
+		super.setRedirect(false);
+		super.setViewPage("/WEB-INF/jsonview.jsp");
+
+	}
+
+}
--- MainSemiProject/src/main/java/yunhwan/model/ProductDAO.java
+++ MainSemiProject/src/main/java/yunhwan/model/ProductDAO.java
@@ -4,12 +4,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+
+
 
 public class ProductDAO implements InterProductDAO {
 
@@ -40,38 +44,75 @@ public class ProductDAO implements InterProductDAO {
 		}
 	}// end of public ProductDAO()---------------------
 
-	
-	
-	// (ALL)전체제품 목록 나타내기 + Ajax(JSON)를 사용하여 전체상품목록을 스크롤 방식으로 페이징 처리하기 위해 제품의 전체 개수 알아오기 //
+	// === Ajax(JSON)를 이용한 더보기 방식(페이징처리)으로 상품정보를 3개씩 잘라서(start ~ end) 조회해오기 === //
 	@Override
-	public int totalAllProductCount(String product_no) throws SQLException {
-		
-		int totalCount = 0;
-		
+	public List<ProductVO> selectProduct(Map<String, String> param) throws SQLException {
+
+		List<ProductVO> prodList = new ArrayList<>();
 		try {
 			conn = ds.getConnection(); // 본인의 오라클DB와 연동
 			
-			String sql = " select count(*) "
+			String sql = " select * "
 						+ " from tbl_product ";
 			
+			
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, product_no);
+			
 			
 			rs =pstmt.executeQuery();
 			
-			rs.next();
+			while (rs.next()) {
+				
+				ProductVO pvo = new ProductVO();
+
+				
+				
+				BuyerTypeVO btvo = new BuyerTypeVO();
+				
+				prodList.add(pvo);
+			}// end of while (rs.next())
 			
-			totalCount = rs.getInt(1);
+			
+			
 				
 		
 		} finally {
 			close();
 		}
-		
-		
-		return totalCount;
-		
-	}// end of public int totalAllProductCount(String string) throws SQLException---------------
+		return prodList;
+	} 
+	
+	
+	// (ALL)전체제품 목록 나타내기 + Ajax(JSON)를 사용하여 전체상품목록을 스크롤 방식으로 페이징 처리하기 위해 제품의 전체 개수 알아오기 //
+//	@Override
+//	public int totalAllProductCount(String product_no) throws SQLException {
+//		
+//		int totalCount = 0;
+//		
+//		try {
+//			conn = ds.getConnection(); // 본인의 오라클DB와 연동
+//			
+//			String sql = " select count(*) "
+//						+ " from tbl_product ";
+//			
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setString(1, product_no);
+//			
+//			rs =pstmt.executeQuery();
+//			
+//			rs.next();
+//			
+//			totalCount = rs.getInt(1);
+//				
+//		
+//		} finally {
+//			close();
+//		}
+//		
+//		
+//		return totalCount;
+//		
+//	}// end of public int totalAllProductCount(String string) throws SQLException---------------
 	
 	
 
