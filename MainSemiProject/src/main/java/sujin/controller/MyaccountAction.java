f56d363041d883a5de67b6775c388d779e90399b
yongyongsujin <jin970510@gmail.com>
2023-04-28 14:17:53 +0900

    용수진 : 로그인 안하면 마이페이지 안보이게 수정 (setRediect 를 false->true)

--- MainSemiProject/src/main/java/sujin/controller/MyaccountAction.java
+++ MainSemiProject/src/main/java/sujin/controller/MyaccountAction.java
@@ -128,18 +128,15 @@ public class MyaccountAction extends AbstractController {
       else {
          // 로그인을 안했을 경우
          
-         String message = "비정상적인 접근입니다. 로그인 후 이용해주세요.";
+    	 String message = "비정상적인 접근입니다. 로그인 후 이용해주세요.";
          String loc = "javascript:history.back()";
-         
+          
          request.setAttribute("message", message);
          request.setAttribute("loc", loc);
-      
-         super.setRedirect(false);
-         super.setViewPage("/WEB-INF/jaesik/msg.jsp");
+       
+         super.setRedirect(true);
+         super.setViewPage(request.getContextPath() + "/index.moc");
       }
-      
-      super.setRedirect(false);
-      super.setViewPage("/WEB-INF/sujin/member/myaccount.jsp");
       
    }
 
