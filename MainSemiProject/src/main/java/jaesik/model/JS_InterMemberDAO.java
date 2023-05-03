80d3272e825c8212e54b6122ab8ba971ed4c221b
SeoJaeSik <sdvilzty@naver.com>
2023-05-03 20:09:28 +0900

    push

--- MainSemiProject/src/main/java/jaesik/model/JS_InterMemberDAO.java
+++ MainSemiProject/src/main/java/jaesik/model/JS_InterMemberDAO.java
@@ -80,5 +80,5 @@ public interface JS_InterMemberDAO {
 
     // 모든 사용자들의 주문내역을 페이징 처리하여 조회해온다.
 	List<HashMap<String, String>> getOrderList(int currentShowPageNo, int sizePerPage) throws SQLException;
-
+ 
 }
